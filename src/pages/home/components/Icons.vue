6be0c64e6964c46878fb3a06972891b4edcdf022
dengjunchao <you@example.com>
2020-02-29 15:50:22 +0800

    index

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="icons">
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="(page,index) of pages" :key="index">
+          <div class="icon" v-for="item of page" :key="item.id">
+            <div class="icon-img">
+              <img class="icon-img-content" :src="item.imgUrl" alt="">
+            </div>
+            <p class="icon-desc">{{item.desc}}</p>
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Icons',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
+      iconList: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/87/20656da0ac042002.png',
+        desc: '天门山公园'
+      }, {
+        id: '0003',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '长沙必游'
+      }, {
+        id: '0004',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+        desc: '张家界公园'
+      }, {
+        id: '0005',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png',
+        desc: '演出'
+      }, {
+        id: '0006',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/fa/2548667cb6e902.png',
+        desc: '动植物园'
+      }, {
+        id: '0007',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/91a810441506568937171df66f754375.png',
+        desc: '红石林'
+      }, {
+        id: '0008',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/b8/c5dcdb58deec2402.png',
+        desc: '凤凰古城'
+      }, {
+        id: '0009',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/87/20656da0ac042002.png',
+        desc: '天门山公园'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varbles.styl"
+  @import "~styles/mixins.styl"
+
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom : 50%
+  .icon
+    position: relative
+    overflow : hidden
+    float : left
+    width : 25%
+    height : 0
+    padding-bottom : 25%
+    .icon-img
+      position : absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing : border-box
+      padding : .1rem
+      .icon-img-content
+        display : block
+        margin : 0 auto
+        height : 100%
+    .icon-desc
+      position : absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height : .44rem
+      line-height : icon-img-content
+      text-align : center
+      color : $darlTextColor
+      ellipsis()
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class='swpier-img' :src="item.imgUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1802/40/40b2075640718d7a3.img.png_600x330_11cd3f65.png'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1803/86/86d65748c963998fa3.water.jpg_600x330_546ce88a.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  //数据穿透 设置样式白色
+  .wrapper >>> .swiper-pagination-bullet-active
+    background : #fff
+  .wrapper
+    width: 100%
+    height: 150px
+    .swpier-img
+      width: 100%
+      height: 150px
+</style>
